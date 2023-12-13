c897dabb5e4a35b16d626c80622983fdf836288e
sfgeekgit <sfgeek@gmail.com>
2023-12-12 19:45:38 -0800

    Building a brain. Poorly

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import copy
 import torch
 import torch.nn as nn 
 
+BOARD_WIDTH = 5
+BOARD_HEIGHT = 5
+LEARNING_RATE = 0.001
 
+INPUT_SIZE = BOARD_WIDTH * BOARD_HEIGHT * 2
+HIDDEN_SIZE = 100
+OUTPUT_SIZE = 8
 
 def initialize_board():    
-    return [[0 for _ in range(5)] for _ in range(5)]
+    return [[0 for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
 
 def display_board_with_labels(board):
-    #print(f"{board=}")
-    #tsor = board_to_tensor(board)
-    #print(f"{tsor=}")
+    print(f"{board=}")
+    tsor = board_to_tensor(board)
+    print(f"{tsor=}")
     col_labels = '  A B C D E'
     print(col_labels)
     for i, row in enumerate(board):
@@ -27,27 +33,22 @@ def board_to_tensor(board):
     seed_tensor = torch.tensor([[1 if cell == 2 else 0 for cell in row] for row in board]).view(-1)
     return torch.cat((dandelion_tensor, seed_tensor))
 
-
-
 def spread_seeds(board, direction):
     new_board = copy.deepcopy(board)
-    for row in range(5):
-        for col in range(5):
+    for row in range(BOARD_HEIGHT):
+        for col in range(BOARD_WIDTH):
             if board[row][col] == 1:
                 dx, dy = direction
                 new_row, new_col = row + dx, col + dy
-                while 0 <= new_row < 5 and 0 <= new_col < 5:
+                while 0 <= new_row < BOARD_HEIGHT and 0 <= new_col < BOARD_WIDTH:
                     if new_board[new_row][new_col] == 0:
                         new_board[new_row][new_col] = 2
                     new_row += dx
                     new_col += dy
     return new_board
 
-
 def check_dandelion_win(board):  # check after every Dandelion turn                                                               
     return all(cell in [1, 2] for row in board for cell in row)
-
-
 
 def convert_user_input(row_str, col_str):
     row = int(row_str) - 1
@@ -58,21 +59,16 @@ def convert_user_input(row_str, col_str):
 directions = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, 1), (-1, -1), (1, 1), (1, -1)]
 direction_names = ['N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SW']
 
-
-
 def validate_row_input(row_str):
-    return row_str.isdigit() and 1 <= int(row_str) <= 5
-
+    return row_str.isdigit() and 1 <= int(row_str) <= BOARD_HEIGHT
 
 def validate_col_input(col_str):
-    return col_str.upper() in ['A', 'B', 'C', 'D', 'E']
-
+    return col_str.upper() in [chr(i + ord('A')) for i in range(BOARD_WIDTH)]
 
 def validate_direction_input(direction_str, available_directions):
     return direction_str.upper() in available_directions
 
 def play_game():
-
     board = initialize_board()
     available_directions = direction_names.copy()
 
@@ -82,15 +78,15 @@ def play_game():
         print("Dandelion's Turn:")
         display_board_with_labels(board)
 
-        row_str = input('Enter the row (1-5) to place the dandelion: ')
+        row_str = input('Enter the row (1-{}) to place the dandelion: '.format(BOARD_HEIGHT))
         while not validate_row_input(row_str):
-            print("Invalid row. Please enter a number between 1 and 5.")
-            row_str = input('Enter the row (1-5) to place the dandelion: ')
+            print("Invalid row. Please enter a number between 1 and {}.".format(BOARD_HEIGHT))
+            row_str = input('Enter the row (1-{}) to place the dandelion: '.format(BOARD_HEIGHT))
 
-        col_str = input('Enter the column (A-E) to place the dandelion: ')
+        col_str = input('Enter the column (A-{}) to place the dandelion: '.format(chr(BOARD_WIDTH + ord('A') - 1)))
         while not validate_col_input(col_str):
-            print("Invalid column. Please enter a letter between A and E.")
-            col_str = input('Enter the column (A-E) to place the dandelion: ')
+            print("Invalid column. Please enter a letter between A and {}.".format(chr(BOARD_WIDTH + ord('A') - 1)))
+            col_str = input('Enter the column (A-{}) to place the dandelion: '.format(chr(BOARD_WIDTH + ord('A') - 1)))
 
         row, col = convert_user_input(row_str, col_str)
         place_dandelion(board, row, col)
@@ -117,8 +113,9 @@ def play_game():
     if not check_dandelion_win(board):
         print('Wind wins!')
 
+class WindNeuralNetwork(nn.Module):
 
-class NeuralNetwork(nn.Module):
+
 
     # might add more layers... Just get prototype working first
 
@@ -144,5 +141,20 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)        
         return logits
 
+def train_nn():
+    wind_brain  = WindNeuralNetwork()
+    optimizer = torch.optim.AdamW(wind_brain.parameters(), lr=LEARNING_RATE)
 
-play_game()
+    board = [[2, 1, 2, 1, 2], [0, 0, 2, 2, 2], [2, 2, 1, 2, 2], [2, 2, 2, 2, 1], [1, 0, 2, 2, 2]]
+    # to do: make a bunch of boards and train on them
+    T = board_to_tensor(board)
+
+    logits = wind_brain(T)  # that calls forward because __call__ is coded magic backend
+    print("logits" , logits)
+
+#####################
+#       Main        #
+
+if __name__ == "__main__":
+    play_game()
+    #train_nn()
